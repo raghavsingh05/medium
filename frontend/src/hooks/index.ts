27957871c9098f9f1ec0229021f91a09ede983fc
raghavsingh05 <raghavsingh47336@gmail.com>
2024-08-28 17:26:58 +0530

    added blogs text

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -2,26 +2,24 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { BACKEND_URL } from "../config";
 
+export const useBlogs = () => {
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState([]);
 
-export const useBlogs = () =>{
-    const [loading, setLoading] = useState(true);
-    const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    axios.get(`${BACKEND_URL}/api/v1/blog/bulk`, {
+        headers: {
+          Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImE2MjE0MjU4LWE4NTAtNGViMy1iYTA1LTgxNjllYWVkNjU4MCJ9.toCGYcEn0MSQ4HEj_ybtIyimPirJiBFhM601jM1Jdc8",
+        },
+      })
+      .then((response) => {
+        setBlogs(response.data.blogs);
+        setLoading(false);
+      });
+  }, []);
 
-    useEffect(() => {
-            axios.get(`${BACKEND_URL}/api/v1/blog/bulk`, {
-                headers: {
-                    Authorization: localStorage.getItem("token")
-                }
-            })
-                .then(response => {
-                    setBlogs(response.data.blogs);
-                    setLoading(false);
-                })
-        }, [])
-
-    return {
-        loading,
-        blogs
-    }
-
-}
+  return {
+    loading,
+    blogs,
+  };
+};
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -3,8 +3,8 @@ import { BlogCard } from "../components/BlogsCards"
 import { useBlogs } from "../hooks"
 
 export const Blogs = () => {
-    const {loading, blogs} = useBlogs();
-    if(loading){
+    const { loading, blogs } = useBlogs();
+    if (loading) {
         return <div >
             loading...
         </div>
@@ -12,15 +12,15 @@ export const Blogs = () => {
     return <div>
         <Appbar />
 
-    <div className=" flex justify-center ">
-        <div className=" max-w-xl ">
-            <BlogCard
-                authorName={"Raghav"}
-                title={"How an Ugly Single-Page website makes $5,000 a Month with affiliate Marketing"}
-                content={"No need to create a fancy and modern website with hundreds of pages to make money online. — Making money online is the dream for man."}
-                publishedDate={"2nd Feb 2024"}
-            />
+        <div className=" flex justify-center ">
+            <div className=" max-w-xl ">
+                <BlogCard
+                    authorName={"Raghav"}
+                    title={"How an Ugly Single-Page website makes $5,000 a Month with affiliate Marketing"}
+                    content={"No need to create a fancy and modern website with hundreds of pages to make money online. — Making money online is the dream for man."}
+                    publishedDate={"2nd Feb 2024"}
+                />
+            </div>
         </div>
-    </div>
     </div>
 }
