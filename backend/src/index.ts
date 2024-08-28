a5507022b4268c59e077fb1268627d4dabe1f8a4
raghavsingh05 <raghavsingh47336@gmail.com>
2024-08-29 02:03:08 +0530

    fixed cors url's

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,22 @@
 import { Hono } from 'hono'
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
-import { cors } from 'hono/cors'
+// import { cors } from 'hono/cors'
 export const app = new Hono<{
   Bindings: {
       DATABASE_URL: string;
       JWT_SECRET: string;
   }
 }>();
-const corsOptions = {
-  origin: 'https://medium-zeta-one.vercel.app/', // Replace with your frontend's domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // If you need to support credentials
-};
+// const corsOptions = {
+//   origin: 'https://medium-zeta-one.vercel.app/', // Replace with your frontend's domain
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true, // If you need to support credentials
+// };
 
-app.use(cors(corsOptions));
-app.use('/*', cors())
+// app.use(cors(corsOptions));
+// app.use('/*', cors())
 app.route('/api/v1/user', userRouter)
 app.route('/api/v1/blog', blogRouter)
 
