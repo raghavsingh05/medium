84bd9cb81e90cef62c27cf9850989096afd8eccb
raghavsingh05 <raghavsingh47336@gmail.com>
2024-08-29 01:29:37 +0530

    fixed type issues

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -71,7 +71,7 @@ interface LabelledInputType {
     onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-function LableInput({ label, placeholder, onChange, type }: LabelledInputType) {
+function LableInput({ label, placeholder, onChange, }: LabelledInputType) {
     return <div>
         <label className="text-left mt-4 block mb-2 text-sm font-semibold text-gray-900 dark:text-black">{label}</label>
         <input onChange={onChange} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder={placeholder} required />
--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -15,7 +15,7 @@ export interface Blog {
 
 export const useBlog = ({id}:{id:string}) =>{
   const [loading, setLoading] = useState(true);
-  const [blog, setBlog] = useState<Blog[]>();
+  const [blog, setBlog] = useState<Blog>();
 
   useEffect(() => {
     axios.get(`${BACKEND_URL}/api/v1/blog/${id}`, {
